f84cbd05ae20f0462a396d8bcbe71dd7704208f9
anbumagan <kishorekumarakk00@gmail.com>
2025-06-25 10:57:13 +0530

    Add framework

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mobilisten",
-            url: "https://raw.githubusercontent.com/anbumagan/MyPrivateSpecs/1.0.2/Mobilisten.zip", checksum: "1ba3ddab33717eda7c5d5e25fd671fc498cbac464fe3864c3730a0068eacb80f")
+            url: "https://raw.githubusercontent.com/anbumagan/MyPrivateSpecs/1.0.2/Mobilisten.zip", 
+			checksum: "1ba3ddab33717eda7c5d5e25fd671fc498cbac464fe3864c3730a0068eacb80f")
     ]
 )
